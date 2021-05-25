d0c7c57b13bbdd5833662f415b0742203b54d2d4
ali hussnain <ali.husnain@dmechs.com>
2021-05-25 19:37:47 +0500

    commented connection

--- personal/app.js
+++ personal/app.js
@@ -18,7 +18,7 @@ const mongoose =require('mongoose');
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-const connection = require("./config/connection");
+// const connection = require("./config/connection");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
