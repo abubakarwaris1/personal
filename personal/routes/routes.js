5319c7334b007041ae81e536c4ee86eea05ebcd8
ali hussnain <ali.husnain@dmechs.com>
2021-05-25 19:44:06 +0500

    added console

--- personal/routes/routes.js
+++ personal/routes/routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 function routes() {
   const users = require("./users");
-
+  console.log('reached in routes');
   router.use("/users", users);
 
   return router;
